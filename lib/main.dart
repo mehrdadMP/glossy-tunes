c6b5988cc3a0ee4952fd614963ff0f338efa9bfb
Mehrdad MoienPour <mehrdad74mp@gmail.com>
2024-02-17 10:58:30 +0330

    Created basic slider

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:glossy_tunes/desktop_view/desktop_main_screen.dart';
 import 'package:glossy_tunes/mobile_view/mobile_main_screen.dart';
 import 'package:glossy_tunes/responsive.dart';
 import 'package:glossy_tunes/tablet_view/tablet_main_screen.dart';
+import 'package:glossy_tunes/wave_slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,10 +20,21 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Responsive(
+      home: Scaffold(
+        body: /* SliderTheme(
+          data: SliderThemeData(trackShape: RectangularSliderTrackShape()),
+          child: Slider(value: 1, onChanged: (value) {
+            
+          },),
+        ), */
+      WaveSlider(
+          sliderWidth: MediaQuery.of(context).size.width,
+          sliderHight: MediaQuery.of(context).size.height), ),
+     
+      /* home: Responsive(
           mobile: MobileMainScreen(),
           tablet: TabletMainScreen(),
-          desktop: DesktopMainScreen()),
+          desktop: DesktopMainScreen()), */
     );
   }
 }
