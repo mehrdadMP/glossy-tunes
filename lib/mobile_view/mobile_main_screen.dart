4c83ec5d58875ce7e4bead9bbc9b0aa4da473a6f
Mehrdad MoienPour <mehrdad74mp@gmail.com>
2024-03-03 03:21:45 +0330

    Added on audio query for fetch all music on storage

--- lib/mobile_view/mobile_main_screen.dart
+++ lib/mobile_view/mobile_main_screen.dart
@@ -377,6 +377,23 @@ class _MusicsList extends StatefulWidget {
 
 class _MusicsListState extends State<_MusicsList> with texts {
   final OnAudioQuery _audioQuery = OnAudioQuery();
+  bool _hasPermission = false;
+
+  @override
+  void initState() {
+    super.initState();
+    LogConfig logConfig = LogConfig(logType: LogType.DEBUG);
+    _audioQuery.setLogConfig(logConfig);
+    checkAndRequestPermissions();
+  }
+
+  checkAndRequestPermissions({bool retry = false}) async {
+    _hasPermission = await _audioQuery.checkAndRequest(
+      retryRequest: retry,
+    );
+    _hasPermission ? setState(() {}) : null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -428,125 +445,152 @@ class _MusicsListState extends State<_MusicsList> with texts {
                   ]),
             ),
             Expanded(
-              child: FutureBuilder<List<SongModel>>(
-                  future: _audioQuery.querySongs(
-                      sortType: null,
-                      uriType: UriType.EXTERNAL,
-                      ignoreCase: true),
-                  builder: (context, item) {
-                    if (item.data == null) {
-                      return Center(child: CircularProgressIndicator());
-                    }
-                    if (item.data!.isEmpty) {
-                      return Center(
-                        child: Text('No songs found'),
-                      );
-                    }
-                    return ListView.builder(
-                      padding: EdgeInsets.zero,
-                      itemCount: 5,
-                      itemBuilder: (context, index) => Padding(
-                        padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
-                        child: Container(
-                          padding: EdgeInsets.fromLTRB(10, 10, 0, 10),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              border: Border.all(
-                                  color: Color.fromARGB(200, 199, 48, 111))),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                SizedBox(
-                                  width: 55,
-                                  height: 55,
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: Image.asset(Assets.images.values
-                                        .elementAt(index)
-                                        .path),
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 2,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+              child: !_hasPermission
+                  ? Center(
+                      child: Text('Permission to storage needed'),
+                    )
+                  : FutureBuilder<List<SongModel>>(
+                      future: _audioQuery.querySongs(
+                          sortType: null,
+                          uriType: UriType.EXTERNAL,
+                          ignoreCase: true),
+                      builder: (context, item) {
+                        if (item.data == null) {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                        if (item.data!.isEmpty) {
+                          return Center(
+                            child: Text('No songs found'),
+                          );
+                        }
+                        return ListView.builder(
+                          padding: EdgeInsets.zero,
+                          itemCount: item.data!.length,
+                          itemBuilder: (context, index) => Padding(
+                            padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
+                            child: Container(
+                              padding: EdgeInsets.fromLTRB(10, 10, 0, 10),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(13),
+                                  border: Border.all(
+                                      color:
+                                          Color.fromARGB(200, 199, 48, 111))),
+                              child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Text(
-                                      'Music name ~ must be...',
-                                      style: textStyle.copyWith(fontSize: 17),
+                                    SizedBox(
+                                      child: ClipRRect(
+                                        child: QueryArtworkWidget(
+                                            nullArtworkWidget: Container(
+                                              decoration: BoxDecoration(
+                                                  color: Color.fromARGB(
+                                                      255, 223, 81, 147),
+                                                  borderRadius:
+                                                      BorderRadius.circular(9)),
+                                              child: Icon(
+                                                CupertinoIcons.music_note_2,
+                                                color: Colors.white,
+                                              ),
+                                              width: 55,
+                                              height: 55,
+                                            ),
+                                            artworkWidth: 55,
+                                            artworkHeight: 55,
+                                            size: 300,
+                                            artworkBorder:
+                                                BorderRadius.circular(9),
+                                            id: item.data![index].id,
+                                            type: ArtworkType.AUDIO),
+                                      ),
                                     ),
-                                    SizedBox(height: 7),
-                                    Row(
-                                      children: [
-                                        Icon(
-                                          CupertinoIcons.device_phone_portrait,
+                                    SizedBox(
+                                      width: 2,
+                                    ),
+                                    SizedBox(width: 175,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            item.data![index].title,
+                                            style:
+                                                textStyle.copyWith(fontSize: 17),
+                                          ),
+                                          SizedBox(height: 7),
+                                          Row(mainAxisAlignment: MainAxisAlignment.start,
+                                            children: [
+                                              Icon(
+                                                CupertinoIcons
+                                                    .device_phone_portrait,
+                                                color: Colors.white,
+                                                size: 20,
+                                              ),
+                                              Text(
+                                                item.data![index]
+                                                    .displayNameWOExt,
+                                                style: textStyle.copyWith(
+                                                    fontSize: 13,
+                                                    color: Colors.white54),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    AudioWave(
+                                        animation: false,
+                                        width: 35,
+                                        height: 30,
+                                        spacing: 3,
+                                        bars: [
+                                          AudioWaveBar(
+                                              heightFactor: 0.5,
+                                              color: Color.fromARGB(
+                                                  132, 255, 255, 255)),
+                                          AudioWaveBar(
+                                              heightFactor: 1,
+                                              color: Color.fromARGB(
+                                                  132, 255, 255, 255)),
+                                          AudioWaveBar(
+                                              heightFactor: 0.4,
+                                              color: Color.fromARGB(
+                                                  132, 255, 255, 255)),
+                                          AudioWaveBar(
+                                              heightFactor: 0.2,
+                                              color: Color.fromARGB(
+                                                  132, 255, 255, 255)),
+                                          AudioWaveBar(
+                                              heightFactor: 0.35,
+                                              color: Color.fromARGB(
+                                                  132, 255, 255, 255)),
+                                          AudioWaveBar(
+                                              heightFactor: 0.8,
+                                              color: Color.fromARGB(
+                                                  132, 255, 255, 255)),
+                                          AudioWaveBar(
+                                              heightFactor: 0.95,
+                                              color: Color.fromARGB(
+                                                  132, 255, 255, 255)),
+                                          AudioWaveBar(
+                                              heightFactor: 0.1,
+                                              color: Color.fromARGB(
+                                                  132, 255, 255, 255)),
+                                        ]),
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: Icon(
+                                          CupertinoIcons.ellipsis_vertical,
                                           color: Colors.white,
-                                          size: 20,
-                                        ),
-                                        Text(
-                                          'Singer name ~ must be here',
-                                          style: textStyle.copyWith(
-                                              fontSize: 13,
-                                              color: Colors.white54),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                AudioWave(
-                                    animation: false,
-                                    width: 35,
-                                    height: 30,
-                                    spacing: 3,
-                                    bars: [
-                                      AudioWaveBar(
-                                          heightFactor: 0.5,
-                                          color: Color.fromARGB(
-                                              132, 255, 255, 255)),
-                                      AudioWaveBar(
-                                          heightFactor: 1,
-                                          color: Color.fromARGB(
-                                              132, 255, 255, 255)),
-                                      AudioWaveBar(
-                                          heightFactor: 0.4,
-                                          color: Color.fromARGB(
-                                              132, 255, 255, 255)),
-                                      AudioWaveBar(
-                                          heightFactor: 0.2,
-                                          color: Color.fromARGB(
-                                              132, 255, 255, 255)),
-                                      AudioWaveBar(
-                                          heightFactor: 0.35,
-                                          color: Color.fromARGB(
-                                              132, 255, 255, 255)),
-                                      AudioWaveBar(
-                                          heightFactor: 0.8,
-                                          color: Color.fromARGB(
-                                              132, 255, 255, 255)),
-                                      AudioWaveBar(
-                                          heightFactor: 0.95,
-                                          color: Color.fromARGB(
-                                              132, 255, 255, 255)),
-                                      AudioWaveBar(
-                                          heightFactor: 0.1,
-                                          color: Color.fromARGB(
-                                              132, 255, 255, 255)),
-                                    ]),
-                                IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      CupertinoIcons.ellipsis_vertical,
-                                      color: Colors.white,
-                                    ))
-                              ]),
-                        ),
-                      ),
-                    );
-                  }),
+                                        ))
+                                  ]),
+                            ),
+                          ),
+                        );
+                      }),
             ),
           ],
         ),
